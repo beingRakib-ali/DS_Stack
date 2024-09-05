534632a54e02ad2fb2eb2d66702a4001ac6cd069
Rakib Sarker <201002196@student.green.ac.bd>
2024-09-06 00:14:48 +0600

    add all

---
+++ Stack_using_Static_array.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+ const int max_size = 500;
+
+class Stack{
+public:
+
+   
+    int a[max_size];
+    int stack_size;
+
+    Stack(){
+        stack_size = 0;
+    }
+
+    void Push(int value)
+    {
+        if (stack_size+1 > max_size)
+        {
+            cout<<"Stack is Full\n";
+            return;
+        }
+        stack_size = stack_size+1;
+        a[stack_size-1] = value;
+
+    }
+
+    void Pop()
+    {
+        if (stack_size == 0)
+        {
+            cout<<"Stack Is Empty\n";
+            return;
+        }
+
+        a[stack_size-1] = 0;
+        stack_size = stack_size - 1;
+        
+    }
+
+    void Top()
+    {
+        if (stack_size == 0)
+        {
+            cout<<"Stack Is Empty\n";
+            return;
+        }
+
+       // return a[stack_size-1];
+       cout<<a[stack_size-1]<<"\n";
+        
+    }
+};
+
+
+int main()
+{
+    Stack st;
+
+    st.Push(5);
+    st.Top();
+
+    st.Push(10);
+    st.Top();
+
+    st.Push(15);
+    st.Top();
+
+    st.Pop();
+    st.Top();
+
+    st.Pop();
+    st.Top();
+    
+    st.Pop();
+    st.Top();
+
+
+
+    return 0;
+}
