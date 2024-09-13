b448233464a0cd4bd4f286dae30ca977a5a79031
Rakib Sarker <201002196@student.green.ac.bd>
2024-09-13 18:34:50 +0600

    add Delete Function

--- stack_using_list_2.cpp
+++ stack_using_list_2.cpp
@@ -28,11 +28,19 @@ void Insert(list<int>&l,int index,int value)
     l.insert(it,value); // push value O(1).
 }
 
+void Delete(list<int>&l,int index)
+{
+    auto it = l.begin();
+    advance(it,index); // traverse index for push value using advance keyward in c++, O(n).
+    l.erase(it); // erase element O(1).
+}
 
 int main()
 {
 
  list<int> l;
+
+  cout<<"Push Element\n";
  // push element in front O(1).
  l.push_front(40);
  l.push_front(30);
@@ -45,8 +53,8 @@ int main()
  l.push_back(60);
  print(l);
 
+ cout<<"Pop Element\n";
  //pop element in front O(1).
-
  l.pop_front();
  l.pop_front();
  print(l);
@@ -57,11 +65,23 @@ int main()
  l.pop_back();
  print(l);
 
-
+ cout<<"Insert Element\n";
 // insert element O(n).
  Insert(l,2,100);
  print(l);
  Insert(l,1,50);
  print(l);
+
+
+
+ cout<<"Delete Element\n";
+ //delete element O(n).
+ Delete(l,2);
+ print(l);
+ Delete(l,2);
+ print(l);
+
+
+
     return 0;
 }
